4d1d8bb4135197c56a720ca3a567613bf8b9e671
DaxDuck <74180238+DaxDuck@users.noreply.github.com>
2022-10-10 20:52:16 +0530

    Create String.cpp

---
+++ cpp/test/String.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main () {
+  string firstName = "John ";
+  string lastName = "Doe";
+  string fullName = firstName + lastName;
+  cout << fullName;
+  return 0;
+}
